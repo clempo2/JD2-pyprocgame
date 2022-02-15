ff5bf515bc7f55584284f348f841fe7798bb5b51
cpi0710 <clement_pellerin@ibi.com>
2022-02-14 20:01:17 -0500

    add jd2.bat

---
+++ jd2.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=C:\P-ROC\JD2-pyprocgame-dev2;C:\P-ROC\pyprocgame-dev;C:\P-ROC\pypinproc-dev;C:\Python27\DLLs;C:\Python27\lib;C:\Python27\lib\lib-tk;C:\Python27;C:\Python27\lib\site-packages;C:\Python27\lib\site-packages\win32;C:\Python27\lib\site-packages\win32\lib;C:\Python27\lib\site-packages\Pythonwin;C:\Python27\lib\site-packages\wx-3.0-msw
+cd C:\P-ROC\JD2-pyprocgame-dev2
+python jd2.py
