9eaffac96f4f6218e7445cbd68a950f7c9509189
Clement Pellerin <clement_pellerin@ibi.com>
2022-04-13 20:09:46 -0400

    edit paths in jd2.bat

--- jd2.bat
+++ jd2.bat
@@ -1,3 +1,3 @@
-set PYTHONPATH=C:\P-ROC\JD2-pyprocgame-dev2;C:\P-ROC\pyprocgame-dev;C:\P-ROC\pypinproc-dev;C:\Python27\DLLs;C:\Python27\lib;C:\Python27\lib\lib-tk;C:\Python27;C:\Python27\lib\site-packages;C:\Python27\lib\site-packages\win32;C:\Python27\lib\site-packages\win32\lib;C:\Python27\lib\site-packages\Pythonwin;C:\Python27\lib\site-packages\wx-3.0-msw
-cd C:\P-ROC\JD2-pyprocgame-dev2
+set PYTHONPATH=C:\P-ROC\JD2-pyprocgame;C:\P-ROC\pyprocgame-dev;C:\P-ROC\pypinproc-dev;C:\Python27\DLLs;C:\Python27\lib;C:\Python27\lib\lib-tk;C:\Python27;C:\Python27\lib\site-packages;C:\Python27\lib\site-packages\win32;C:\Python27\lib\site-packages\win32\lib;C:\Python27\lib\site-packages\Pythonwin;C:\Python27\lib\site-packages\wx-3.0-msw
+cd C:\P-ROC\JD2-pyprocgame
 python jd2.py
